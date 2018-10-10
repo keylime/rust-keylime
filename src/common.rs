6f293d9817504946804ae2ccbfeb20f622f7d18d
Robbie Harwood <rharwood@redhat.com>
2018-10-10 14:29:20 -0400

    Apply a vague style and clean up

--- src/common.rs
+++ src/common.rs
@@ -1,6 +1,3 @@
-// use std::vec::Vec<&str>;
-// use std::string::String;
-
 extern crate futures;
 extern crate hyper;
 extern crate serde_json;
@@ -13,99 +10,89 @@ use std::collections::HashMap;
  * 
  * Parameters: code number, status string, content string
  * Return: Combine all information into a Response struct
- *
  */
-pub fn json_response_content(code: i32, status: String, results: String) -> Response<Body> {
-	// let c = code.to_string();
-	let data = vec![code.to_string(), status, results];
+pub fn json_response_content(code: i32, status: String, results: String)
+                             -> Response<Body> {
+    let data = vec![code.to_string(), status, results];
 
-	let res = match serde_json::to_string(&data){
-		Ok(json) => {
-			// return a json response
-			Response::builder()
-				.header(header::CONTENT_TYPE, "application/json")
-				.body(Body::from(json))
-				.unwrap()
-		}
+    match serde_json::to_string(&data){
+        Ok(json) => {
+            // return a json response
+            Response::builder()
+                .header(header::CONTENT_TYPE, "application/json")
+                .body(Body::from(json))
+                .unwrap()
+        }
 
-		// This is unnecessary here，probably won't fail
-		Err(e) => {
-			eprintln!("serializing json: {}", e);
+        // This is unnecessary here，probably won't fail
+        Err(e) => {
+            eprintln!("serializing json: {}", e);
 
-			Response::builder()
-				.status(StatusCode::INTERNAL_SERVER_ERROR)
-				.body(Body::from("Internal Server Error"))
-				.unwrap()
-		}
-	};
-
-	res
+            Response::builder()
+                .status(StatusCode::INTERNAL_SERVER_ERROR)
+                .body(Body::from("Internal Server Error"))
+                .unwrap()
+        }
+    }
 }
 
 
 /*
- * seperate url path by '/', first element is dropped since it is an empty string
+ * seperate url path by '/', first element is dropped since it is an empty
+ * string
  *
  * Paramters: string and delimiter
  * return: Vector of string contains the path content in original order
- *
- *
- *
  */
 pub fn string_split_by_seperator(data: &str, seperator: char) -> Vec<&str> {
-	let mut v: Vec<&str> = data.split(seperator).collect();
-	v.remove(0);
-	v
+    let mut v: Vec<&str> = data.split(seperator).collect();
+    v.remove(0);
+    v
 }
 
 
 // hashmap doesn't own the parameters, just borrow it
 
 /*
- * convert a api resquest path to a map that contains the key and value in pair
- * from the original api request
+ * convert a api resquest path to a map that contains the key and value in
+ * pair from the original api request
  *
  * Parameters: api string
  * Return: map with api key and value
  */
 pub fn get_restful_parameters(urlstring: &str) -> HashMap<&str, &str> {
-	let list = string_split_by_seperator(urlstring, '/');
-	let mut parameters = HashMap::new();
-	let (_, right) = list[0].split_at(1);
-	parameters.insert("api_version", right);
+    let list = string_split_by_seperator(urlstring, '/');
+    let mut parameters = HashMap::new();
+    let (_, right) = list[0].split_at(1);
+    parameters.insert("api_version", right);
 
-	let mut iter = 1..list.len();
-	loop {
-		match iter.next() {
-			Some(x) => {
-				parameters.insert(list[x], list[x + 1]);
-			},
-			None => break,
-		}
-		iter.next();
-	}
-	parameters
+    /* TODO comment why this drops the first element */
+    for x in 1..(list.len() - 1) {
+        parameters.insert(list[x], list[x + 1]);
+    }
+    parameters
 }
 
 
 // Unit Testing
 #[cfg(test)]
 mod tests {
-	use super::*;
+    use super::*;
 
     #[test]
     fn test_split_string() {
-        assert_eq!(string_split_by_seperator("/v2/verify/pubkey", '/'), ["v2", "verify", "pubkey"]);
+        assert_eq!(string_split_by_seperator("/v2/verify/pubkey", '/'),
+                   ["v2", "verify", "pubkey"]);
     }
 
     #[test]
     fn test_get_restful_parameters() {
-    	let mut map = HashMap::new();
-    	map.insert("verify", "pubkey");
-    	map.insert("api_version", "2");
+        let mut map = HashMap::new();
+        map.insert("verify", "pubkey");
+        map.insert("api_version", "2");
 
-    	// "{"api_version": "v2", "verify": "pubkey"}"	
-    	assert_eq!(get_restful_parameters("/v2/verify/pubkey"), map);
+        // "{"api_version": "v2", "verify": "pubkey"}"  
+        assert_eq!(get_restful_parameters("/v2/verify/pubkey"), map);
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,11 @@ extern crate hyper;
 extern crate pretty_env_logger;
 
 mod common;
-use common::*;
 
 use futures::future;
-use hyper::rt::{Future, Stream};
+use hyper::rt::Future;
 use hyper::service::service_fn;
 use hyper::{Body, Method, Request, Response, Server, StatusCode};
-use std::collections::HashMap;
 
 type BoxFut = Box<Future<Item = Response<Body>, Error = hyper::Error> + Send>;
 
@@ -20,133 +18,149 @@ fn main() {
 
     pretty_env_logger::init();
 
-    // let addr = ([127.0,0.1], 3000).into();
-	let addr = "127.0.0.1:1337".parse().unwrap();
+    /* Should be port 3000 eventually */
+    let addr = "127.0.0.1:1337".parse().unwrap();
 
     let server = Server::bind(&addr)
-    	.serve(|| service_fn(response_function))
-    	.map_err(|e| eprintln!("server erro: {}", e));
+        .serve(|| service_fn(response_function))
+        .map_err(|e| eprintln!("server erro: {}", e));
 
     println!("Listening no http://{}", addr);
 
-    // run server for-ever
+    // run server forever
     hyper::rt::run(server);
 }
 
 
 fn response_function(req: Request<Body>) -> BoxFut {
+    let mut res = Response::default();
+    
+    // need a method to process input api path !!
+    let parameters = common::get_restful_parameters(req.uri().path());
 
-	let mut res = Response::default();
-	
-	// need a method to process input api path !!
-	let mut parameters = common::get_restful_parameters(req.uri().path());
+    match req.method() {
+        &Method::GET => {
+            // keys request 
+            if parameters.contains_key("keys") {
+                match parameters.get(&"keys") {
+                    // check Kb value is available to perform the do_hmac
+                    // crypto request
+                    // verify will do hmac for the challenge
+                    // orignal: crypto.do_hmac(self.server.K, challenge) 
+                    Some(&"verify") => {
+                        /* /keys/verify/challenge/blablabla */
+                        let _challenge = parameters.get(&"challenge");
+                        let hmac_result = String::from("hmac placeholder");
+                        res = common::json_response_content(
+                            200, "Success".to_string(), hmac_result);
+                    }
 
-	match req.method() {
+                    // pubkey export the rsa pub key
+                    // original: self.server.rsapublickey_exportable
+                    Some(&"pubkey") => {
+                        /* /keys/pubkey/ */
+                        let pubkey_placeholder =
+                            String::from("pubkey_placeholder");
+                        res = common::json_response_content(
+                            200, "Success".to_string(), pubkey_placeholder);
+                    }
 
-		&Method::GET => {
+                    _ => {
+                        res = common::json_response_content(
+                            400, "Fail".to_string(),
+                            "uri is not supported".to_string());
+                    }
+                };
 
-			// keys request 
-			if parameters.contains_key("keys") {
-				match parameters.get(&"keys") {
-					// check Kb value is available to perform the do_hmac crypto request
+            // quote resques
+            // tpm implementation need for quote
 
-					// verify will do hmac for the challenge
-					// orignal: crypto.do_hmac(self.server.K, challenge) 
-					Some(&"verify") => {          /* /keys/verify/challenge/blablabla */
-						let challenge = parameters.get(&"challenge");
-						let hmac_result = String::from("hmac placeholder");
-						res = common::json_response_content(200, "Success".to_string(), hmac_result);
-					}
+            // response include quote and ima_measurement_list
+            } else if parameters.contains_key("quotes") {
+                let _nouce = parameters.get(&"nouce").unwrap();
 
-					// pubkey export the rsa pub key
-					// original: self.server.rsapublickey_exportable
-					Some(&"pubkey") => {         /* /keys/pubkey/ */
-						let pubkey_placeholder = String::from("pubkey_placeholder");
-						res = common::json_response_content(200, "Success".to_string(), pubkey_placeholder);
-					}
+                // only one of these two is available, the other is None if it
+                // is not in the HashMap
+                let pcr_mask = parameters.get(&"mask").unwrap();
+                let vpcr_mask = parameters.get(&"vmask").unwrap();
+                let mut ima_mask: &str;
 
-					_ => {
-						res = common::json_response_content(400, "Fail".to_string(), "uri is not supported".to_string());
-					}
-				};
+                match parameters.get(&"quotes"){
+                    Some(&"identity") => {
+                        // vtpm option
+                        ima_mask = vpcr_mask; // take ownership
+                    }
 
-			// quote resques
-			// tpm implementation need for quote
+                    _ => {
+                        ima_mask = pcr_mask;
+                    }
+                }
 
-			// response include quote and ima_measurement_list
-			}else if parameters.contains_key("quotes") {
-				let nouce = parameters.get(&"nouce").unwrap();
+                println!("Now it should use ima_mask: {} to check with tpm",
+                         ima_mask);
+                res = common::json_response_content(
+                    400, "Fail".to_string(),
+                    "Check with tpm using imaMask".to_string());
+            }
 
-				// only one of these two is available, the other is None if it is not in the HashMap
-				let pcrMask = parameters.get(&"mask").unwrap();
-				let vpcrMask = parameters.get(&"vmask").unwrap();
-				let mut imaMask: &str;
+            // else if parameters.contains_key("showkeys") {
+            //  println!("U: {}, V: {}", U, V);;
+            // }
+            else {
+                res = common::json_response_content(
+                    400, "Fail".to_string(),
+                    "uri is not supported".to_string());
+            }
+        }
 
-				match parameters.get(&"quotes"){
-					Some(&"identity") => {
-						// vtpm option
-						imaMask = vpcrMask; // take ownership
-					}
+        &Method::POST => {
+            match parameters.get(&"keys") {
+                Some(&"ukey") => {
+                    res = common::json_response_content(
+                        400, "Success".to_string(),
+                        "u key added".to_string()); 
+                    println!("adding u key");
+                }
 
-					_ => {
-						imaMask = pcrMask;
-					}
-				}
+                Some(&"vkey") => {
+                    res = common::json_response_content(
+                        400, "Success".to_string(),
+                        "v key added".to_string());
+                    println!("adding v key");
+                }
 
-				println!("Now it should use imaMask: {} to check with tpm", imaMask);
-				let res = common::json_response_content(400, "Fail".to_string(),"Check with tpm using imaMask".to_string());
-			}
+                _ => {
+                    res = common::json_response_content(
+                        400, "Fail".to_string(),
+                        "uri not supported".to_string());
+                }
+            }
+        }
 
-			// else if parameters.contains_key("showkeys") {
-			// 	println!("U: {}, V: {}", U, V);;
-			// }
-			else{
-				let res = common::json_response_content(400, "Fail".to_string(), "uri is not supported".to_string());
-			}
-		}
+        _  =>  {
+            let body = Body::from(NOTFOUND);
+            res = Response::builder()
+                .status(StatusCode::NOT_FOUND)
+                .body(body)
+                .unwrap();
+        }
 
-		&Method::POST => {
-			match parameters.get(&"keys") {
-				Some(&"ukey") => {
-					let res = common::json_response_content(400, "Success".to_string(), "u key added".to_string());	
-					println!("adding u key");
-				}
+    }
 
-				Some(&"vkey") => {
-					let res = common::json_response_content(400, "Success".to_string(), "v key added".to_string());
-					println!("adding v key");
-				}
-
-				_ => {
-					let res = common::json_response_content(400, "Fail".to_string(), "uri not supported".to_string());
-				}
-			}
-		}
-
-		_  =>  {
-			let body = Body::from(NOTFOUND);
-			res = Response::builder()
-				.status(StatusCode::NOT_FOUND)
-				.body(body)
-				.unwrap();
-		}
-
-	}
-
-	Box::new(future::ok(res))
+    Box::new(future::ok(res))
 }
 
 #[cfg(test)]
 mod tests {
-	use super::*;
+    use super::*;
 
     #[test]
     fn test_get_restful_parameters() {
-    	let mut map = HashMap::new();
-    	map.insert("verify", "pubkey");
-    	map.insert("api_version", "2");
+        let mut map = HashMap::new();
+        map.insert("verify", "pubkey");
+        map.insert("api_version", "2");
 
-    	// "{"api_version": "v2", "verify": "pubkey"}"	
-    	assert_eq!(common::get_restful_parameters("/v2/verify/pubkey"), map);
+        // "{"api_version": "v2", "verify": "pubkey"}"  
+        assert_eq!(common::get_restful_parameters("/v2/verify/pubkey"), map);
     }
 }
