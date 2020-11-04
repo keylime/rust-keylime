36802e4694fe3b69eebbb080df48570d46214a04
Patrick Uiterwijk <patrick@puiterwijk.org>
2020-11-04 13:09:10 +0100

    Make tpm.rs use the crate Result
    Signed-off-by: Patrick Uiterwijk <patrick@puiterwijk.org>

--- src/tpm.rs
+++ src/tpm.rs
@@ -1,10 +1,11 @@
 use std::str::FromStr;
 
-use tss_esapi::constants::algorithm::HashingAlgorithm;
-use tss_esapi::constants::tss as tss_constants;
-use tss_esapi::tss2_esys::{ESYS_TR, ESYS_TR_NONE, ESYS_TR_RH_OWNER};
-use tss_esapi::Context;
-use tss_esapi::Tcti;
+use crate::Result;
+
+use tss_esapi::{
+    constants::{algorithm::HashingAlgorithm, tss::*},
+    Context, Tcti,
+};
 
 /*
  * Input: None
@@ -13,7 +14,7 @@ use tss_esapi::Tcti;
  * Example call:
  * let mut ctx = tpm::get_tpm2_ctx();
  */
-pub(crate) fn get_tpm2_ctx() -> Result<tss_esapi::Context, tss_esapi::Error> {
+pub(crate) fn get_tpm2_ctx() -> Result<tss_esapi::Context> {
     let tcti_path = if std::path::Path::new("/dev/tpmrm0").exists() {
         "device:/dev/tpmrm0"
     } else {
@@ -21,5 +22,5 @@ pub(crate) fn get_tpm2_ctx() -> Result<tss_esapi::Context, tss_esapi::Error> {
     };
 
     let tcti = Tcti::from_str(tcti_path)?;
-    unsafe { Context::new(tcti) }
+    unsafe { Context::new(tcti) }.map_err(|e| e.into())
 }
