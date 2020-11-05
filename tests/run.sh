2added70c8b7b7f06f318ee27ed76549301afcf0
Patrick Uiterwijk <patrick@puiterwijk.org>
2020-11-05 14:57:18 +0100

    Move to a Fedora container for CI
    Signed-off-by: Patrick Uiterwijk <patrick@puiterwijk.org>

---
+++ tests/run.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -euf -o pipefail
+
+echo "-------- Setting up Virtual TPM"
+mkdir /tmp/tpmdir
+swtpm_setup --tpm2 \
+    --tpmstate /tmp/tpmdir \
+    --createek --allow-signing --decryption --create-ek-cert \
+    --create-platform-cert \
+    --display
+swtpm socket --tpm2 \
+    --tpmstate dir=/tmp/tpmdir \
+    --flags startup-clear \
+    --ctrl type=tcp,port=2322 \
+    --server type=tcp,port=2321 \
+    --daemon
+tpm2-abrmd \
+    --logger=stdout \
+    --tcti=swtpm: \
+    --allow-root \
+    --session \
+    --flush-all &
+
+echo "-------- Running clippy"
+# The denies are currently disabled, because that will require a bunch of code cleanup
+cargo clippy --all-targets --all-features #  -- -D clippy::all -D clippy::cargo
+
+echo "-------- Building"
+RUST_BACKTRACE=1 cargo build
+
+echo "-------- Testing"
+TEST_TCTI=tabrmd:bus_type=session RUST_BACKTRACE=1 RUST_LOG=info cargo test -- --nocapture
