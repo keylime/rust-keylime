837bd667be80613c91c53c11a2b13c063d048d1a
Luke Hinds <lhinds@redhat.com>
2020-12-02 15:52:30 +0000

    All unwraps (in test code)

--- src/main.rs
+++ src/main.rs
@@ -56,7 +56,7 @@ fn get_uuid(agent_uuid_config: &str) -> String {
 fn test_get_uuid() {
     assert_eq!(get_uuid("openstack"), "openstack");
     assert_eq!(get_uuid("hash_ek"), "hash_ek");
-    Uuid::parse_str(&get_uuid("generate")).unwrap();
+    Uuid::parse_str(&get_uuid("generate")).unwrap(); //#[allow_ci]
     assert_eq!(
         get_uuid("D432FBB3-D2F1-4A97-9EF7-75BD81C00000"),
         "d432fbb3-d2f1-4a97-9ef7-75bd81c00000"
@@ -66,7 +66,7 @@ fn test_get_uuid() {
         "d432fbb3-d2f1-4a97-9ef7-75bd81c0000X"
     );
     Uuid::parse_str(&get_uuid("D432FBB3-D2F1-4A97-9EF7-75BD81C0000X"))
-        .unwrap();
+        .unwrap(); //#[allow_ci]
 }
 
 #[actix_web::main]
@@ -108,9 +108,9 @@ async fn main() -> Result<()> {
     let ek = "-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAmYicaAAArcin6fRZmzkc\nssyW5VFDWuB+FXF1HdEmJR4jEMdhlp8H9uAMwExY/+6aujElLJgBSKYPPeC7d/nI\nUIYc71oBxQEn6l3DTJO+1Nl1Wq6xYvlGrJMcuAFlznJCo0IF3MVLd45zEgdDmG5T\ntQ+EMAl64eC+aIG9Zp6InLbuZd3oisjE16TiK4Rg5dHAnfU6YSo9CIVSGw6PuCqX\n3aEyeikKNLGwX7ENp+fIVxj9Y00I5JoxDgD9ufLF7V55JVXKdZ0F51NghMyJRSt+\nkyEiqHPRRVTHw+248uziY4ioaDb3EBNKjnC/xcAdUoNxE4I1W8IW8UF/4td/AU11\nfwIDAQAB\n-----END PUBLIC KEY-----\n";
     let aik = "-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAvRI0YcjxLLHFzBmVxO6I\nwuGri5ejIBaCb/XqirXgryXQYATmStk4WgdaDbZtW8JuLdYiTQftCFSP9iIo5jGA\nU443s/zXlWv15raraOsRlSdtSXAKv4dUFOc18GvyiL4ubrkHF5MnbdDlNoG5gZNS\nBV5/MIka5h4p7fpji93neoHNjbm/L3rSPknJnX4TVdxeOOV5izbkfD2TYD+rZ2nx\nzxqox/++dtl4yZ8jLEQnGAyFPMhMLHg8uZKNki2+DPM1oB7hKCHcG3QspVi4mOCc\nvDBt5+2VHakDi8dfFeTncHAQFqAWAHQfvYTo+EUPZpP3+49zBll/DnGt2He/GEvT\nFwIDAQAB\n-----END PUBLIC KEY-----\n";
     let ekcert = "MIID8zCCAlugAwIBAgIBIDANBgkqhkiG9w0BAQsFADAYMRYwFAYDVQQDEw1zd3RwbS1sb2NhbGNhMB4XDTIwMTIwMTExMzgwMVoXDTMwMTEyOTExMzgwMVowEjEQMA4GA1UEAxMHdW5rbm93bjCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAMLEHnYAAWJY7AQ3pf5Iq08PNYtZfBVU2cVqrgR18oc5tjFxORObM6haNTfMQLge9wqbpB/jdMzTipIUh6GwIqDuioLKbe1Ag7yJHYmBZuFq5j/EY4IGH9+Zy2mvTVDIkx1zV0zDCopliZC6pmFJtVA6oRQD1drIlMFAb7oX5SilqQE+++PWR9BCEmFxpRla32F/ejcFIbP1+DhtwSPXbXGcw885FjCjOlPhXTeDdXXkipf9KtKHblfFsx1DD4hEviiYqt1XwPtgOZlabIsHV+KLHRzLGvjUtVmOP4QKY00LfhFH9b0+8vCIRBlhdISMXWDiv8QIxm2iq/jSGPdPJo8CAwEAAaOBzTCByjAQBgNVHSUECTAHBgVngQUIATBSBgNVHREBAf8ESDBGpEQwQjEWMBQGBWeBBQIBDAtpZDowMDAwMTAxNDEQMA4GBWeBBQICDAVzd3RwbTEWMBQGBWeBBQIDDAtpZDoyMDE3MDYxOTAMBgNVHRMBAf8EAjAAMCIGA1UdCQQbMBkwFwYFZ4EFAhAxDjAMDAMyLjACAQACAgCWMB8GA1UdIwQYMBaAFNZCf4X9wlnqDfz4wdpgGhA40JZIMA8GA1UdDwEB/wQFAwMHIAAwDQYJKoZIhvcNAQELBQADggGBAKqAAL6rA9JotB5jVr4JiCT3tuKKMHx0dIn2ioYPd7LWahWKcgCgSWqrRziDOVgoTGcWp+Jc2exQ0Rsle3SrdY+Vp4EODsIN3TRkJ0HtzP6NoacEJheKkAdcMhfJfDCam25Wdv1unIDbbwkjSqYR4qfiPOKTFIH4+Dl+uypuvECw5OAyMG64EDTV5aWKtNYkYf7h60Yo77gYNmAP6XJhk2QrMKhWH4RsJvibcXp5CC7dhJEvnCUAnRE85zS3eoppaxssfd+4EIcCqQRCY7a9kIAKBGtemIFgkOwrynxhSBjpMSfcU99tsuNnMnSOhqJRLoJ3wKncTaBiJbzJDwuzPAOYNoBTOiBWLQNThv0Ky4KyuLFkxWM9Q3zcecpxR2PL0hVskB7c0H5+urwDid6SZ7VZ7nWY0z3WexqUY6xRp1C6BtBGVeh6w3Vlu60c31k89mR6eC6K1j/iNHXBdvgGPqH23wPhOr5MeB+g+UOOSaR+EYkr7NF+ekUvItXtB/tY7Q==";
-    let ekcert = base64::decode(ekcert).unwrap();
+    let ekcert = base64::decode(ekcert).unwrap(); //#[allow_ci]
     let ek_tpm = "AToAAQALAAMAsgAgg3GXZ0SEs/gakMyNRqXXJP1S124GUgtk8qHaGzMUaaoABgCAAEMAEAgAAAAAAAEAmYicaAAArcin6fRZmzkcssyW5VFDWuB+FXF1HdEmJR4jEMdhlp8H9uAMwExY/+6aujElLJgBSKYPPeC7d/nIUIYc71oBxQEn6l3DTJO+1Nl1Wq6xYvlGrJMcuAFlznJCo0IF3MVLd45zEgdDmG5TtQ+EMAl64eC+aIG9Zp6InLbuZd3oisjE16TiK4Rg5dHAnfU6YSo9CIVSGw6PuCqX3aEyeikKNLGwX7ENp+fIVxj9Y00I5JoxDgD9ufLF7V55JVXKdZ0F51NghMyJRSt+kyEiqHPRRVTHw+248uziY4ioaDb3EBNKjnC/xcAdUoNxE4I1W8IW8UF/4td/AU11fw==";
-    let ek_tpm = base64::decode(ek_tpm).unwrap();
+    let ek_tpm = base64::decode(ek_tpm).unwrap(); //#[allow_ci]
     let aik_name = "000bb35615c533f39df1f1a30d35a42c2f9dc5b8a6a3c5332ec59e702d8e04a288fa";
     // placeholder END
     let mut keyblob = registrar_agent::do_register_agent(
