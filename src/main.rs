1b83866558bccee3d5360e1771e2affa2416ae33
Luke Hinds <lhinds@redhat.com>
2021-02-03 13:11:05 +0000

    Basic quote handlers
    The following code replicates python agent code:
    
    https://github.com/keylime/keylime/blob/d987c71dd878492ac2b59eca781009fe270d219a/keylime/keylime_agent.py#L95-L114
    
    This is more or a less at the point that we can call the tpm for quote.
    
    Signed-off-by: Luke Hinds <lhinds@redhat.com>

--- src/main.rs
+++ src/main.rs
@@ -86,6 +86,10 @@ async fn main() -> Result<()> {
                 web::resource("/quotes/identity")
                     .route(web::get().to(quotes_handler::identity)),
             )
+            .service(
+                web::resource("/quotes/integrity")
+                    .route(web::get().to(quotes_handler::integrity)),
+            )
     })
     .bind(format!("{}:{}", cloudagent_ip, cloudagent_port))?
     .run()
