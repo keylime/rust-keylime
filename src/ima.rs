807b371d86ae8e6f4c6b303fca103422e2f32187
Anderson Toshiyuki Sasaki <ansasaki@redhat.com>
2022-02-22 16:19:44 +0100

    ima: Avoid using unwrap() or panic!()
    This also makes changes to the rust iterator fold method and to avoid
    cloning when possible.
    
    Mark valid uses of unwrap() in tests with //#[allow_ci]
    
    Signed-off-by: Anderson Toshiyuki Sasaki <ansasaki@redhat.com>

--- src/ima.rs
+++ src/ima.rs
@@ -1,18 +1,13 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright 2021 Keylime Authors
 
-#[macro_use]
 use log::*;
-
-use std::collections::HashSet;
-use std::fs::File;
-use std::io::prelude::*;
-use std::io::Error;
-use std::io::SeekFrom;
-use std::path::Path;
-use std::str;
-
-use tempfile::NamedTempFile;
+use std::{
+    collections::HashSet,
+    fs::File,
+    io::{prelude::*, Error, SeekFrom},
+    path::Path,
+};
 
 /// IMAMeasurementList models the IMA measurement lists's last two known
 /// numbers of entries in the log and filesizes at that point
@@ -25,33 +20,31 @@ pub type IMAError = Result<(String, u64, u64), Error>;
 
 impl ImaMeasurementList {
     pub(crate) fn new() -> ImaMeasurementList {
-        let mut iml = ImaMeasurementList {
+        ImaMeasurementList {
             entries: HashSet::new(),
-        };
-        let _ = iml.reset();
-        iml
+        }
     }
 
     fn reset(&mut self) {
         self.entries = HashSet::new();
     }
 
-    fn update(&mut self, num_entries: u64, filesize: u64) {
+    fn update(&mut self, num_entries: u64, filesize: u64) -> Option<bool> {
         if self.entries.len() > 32 {
-            let e = self.entries.iter().next().cloned().unwrap();
+            let e = *self.entries.iter().next()?;
             let _ = self.entries.remove(&e);
         }
-        let _ = self.entries.insert((num_entries, filesize));
+        Some(self.entries.insert((num_entries, filesize)))
     }
 
     fn find(&self, nth_entry: u64) -> (u64, u64) {
-        let mut best = (0, 0);
-        for entry in self.entries.iter() {
+        self.entries.iter().fold((0, 0), |best, entry| {
             if entry.0 > best.0 && entry.0 < nth_entry {
-                best = *entry
+                *entry
+            } else {
+                best
             }
-        }
-        best
+        })
     }
 }
 
@@ -67,6 +60,12 @@ pub(crate) fn read_measurement_list(
     filename: &Path,
     nth_entry: u64,
 ) -> IMAError {
+    if !Path::new(filename).exists() {
+        let _ = ima_ml.reset();
+        warn!("IMA measurement list not available: {}", filename.display());
+        return Ok(("".to_string(), 0, 0));
+    }
+
     let mut nth_entry = nth_entry;
 
     // Try to find the closest entry to the nth_entry
@@ -74,82 +73,65 @@ pub(crate) fn read_measurement_list(
 
     let mut ml = None;
     let mut filedata = String::new();
+    let mut file = File::open(filename)?;
+    let _ = file.seek(SeekFrom::Start(filesize))?;
+    let _ = file.read_to_string(&mut filedata)?;
+    let mut offset: usize = 0;
 
-    if !Path::new(filename).exists() {
-        let _ = ima_ml.reset();
-        nth_entry = 0;
-        log::warn!(
-            "IMA measurement list not available: {}",
-            filename.to_str().unwrap()
-        );
-    } else {
-        let mut file = File::open(filename)?;
-        let _ = file.seek(SeekFrom::Start(filesize))?;
-        let _ = file.read_to_string(&mut filedata)?;
-
-        let mut offset: usize = 0;
-        loop {
-            if nth_entry == num_entries {
-                ml = Some(&filedata[offset..]);
-            }
-            let s = &filedata[offset..];
-            let idx = match s.find('\n') {
-                None => break,
-                Some(i) => i,
-            };
-            offset = offset + idx + 1;
-            num_entries += 1;
+    loop {
+        if nth_entry == num_entries {
+            ml = Some(&filedata[offset..]);
         }
-        ima_ml.update(num_entries, filesize + offset as u64);
-
-        match ml {
-            None => return read_measurement_list(ima_ml, filename, 0),
-            Some(_) => (),
-        }
+        let s = &filedata[offset..];
+        let idx = match s.find('\n') {
+            None => break,
+            Some(i) => i,
+        };
+        offset = offset + idx + 1;
+        num_entries += 1;
     }
-    Ok((String::from(ml.unwrap()), nth_entry, num_entries))
+
+    let _ = ima_ml.update(num_entries, filesize + offset as u64);
+
+    match ml {
+        None => read_measurement_list(ima_ml, filename, 0),
+        Some(slice) => Ok((String::from(slice), nth_entry, num_entries)),
+    }
 }
 
-#[test]
-fn read_measurement_list_test() {
-    let mut ima_ml = ImaMeasurementList::new();
+mod tests {
+    use super::*;
+    use tempfile::NamedTempFile;
 
-    let filedata = "0-entry\n1-entry\n2-entry\n";
-    let mut tf = match NamedTempFile::new() {
-        Ok(tf) => tf,
-        Err(_) => panic!("Could not create temp file"),
-    };
-    tf.write_all(filedata.as_bytes());
-    tf.flush();
+    #[test]
+    fn read_measurement_list_test() {
+        let mut ima_ml = ImaMeasurementList::new();
 
-    // Request the 2nd entry, which is available
-    let (ml, nth_entry, num_entries) =
-        match read_measurement_list(&mut ima_ml, tf.path(), 2) {
-            Err(v) => panic!("Reading measurement list failed: {}", v),
-            Ok((ml, nth_entry, num_entries)) => (ml, nth_entry, num_entries),
-        };
-    assert_eq!(num_entries, 3);
-    assert_eq!(nth_entry, 2);
-    assert_eq!(ml.find("2-entry").unwrap(), 0);
+        let filedata = "0-entry\n1-entry\n2-entry\n";
+        let mut tf = NamedTempFile::new().unwrap(); //#[allow_ci]
+        tf.write_all(filedata.as_bytes());
+        tf.flush();
 
-    // Request the 3rd entry, which is not available yet, thus we get an empty list
-    let (ml, nth_entry, num_entries) =
-        match read_measurement_list(&mut ima_ml, tf.path(), 3) {
-            Err(v) => panic!("Reading measurement list failed: {}", v),
-            Ok((ml, nth_entry, num_entries)) => (ml, nth_entry, num_entries),
-        };
-    assert_eq!(num_entries, 3);
-    assert_eq!(nth_entry, 3);
-    assert_eq!(ml.len(), 0);
+        // Request the 2nd entry, which is available
+        let (ml, nth_entry, num_entries) =
+            read_measurement_list(&mut ima_ml, tf.path(), 2).unwrap(); //#[allow_ci]
+        assert_eq!(num_entries, 3);
+        assert_eq!(nth_entry, 2);
+        assert_eq!(ml.find("2-entry").unwrap(), 0); //#[allow_ci]
 
-    // Request the 4th entry, which is beyond the next entry; since this is wrong,
-    // we expect the entire list now.
-    let (ml, nth_entry, num_entries) =
-        match read_measurement_list(&mut ima_ml, tf.path(), 4) {
-            Err(v) => panic!("Reading measurement list failed: {}", v),
-            Ok((ml, nth_entry, num_entries)) => (ml, nth_entry, num_entries),
-        };
-    assert_eq!(num_entries, 3);
-    assert_eq!(nth_entry, 0);
-    assert_eq!(ml.find("0-entry").unwrap(), 0);
+        // Request the 3rd entry, which is not available yet, thus we get an empty list
+        let (ml, nth_entry, num_entries) =
+            read_measurement_list(&mut ima_ml, tf.path(), 3).unwrap(); //#[allow_ci]
+        assert_eq!(num_entries, 3);
+        assert_eq!(nth_entry, 3);
+        assert_eq!(ml.len(), 0);
+
+        // Request the 4th entry, which is beyond the next entry; since this is wrong,
+        // we expect the entire list now.
+        let (ml, nth_entry, num_entries) =
+            read_measurement_list(&mut ima_ml, tf.path(), 4).unwrap(); //#[allow_ci]
+        assert_eq!(num_entries, 3);
+        assert_eq!(nth_entry, 0);
+        assert_eq!(ml.find("0-entry").unwrap(), 0); //#[allow_ci]
+    }
 }
