284d5124c70dc18b0a2c121ed1867f84654ee25d
Anderson Toshiyuki Sasaki <ansasaki@redhat.com>
2022-04-08 16:48:07 +0200

    errors_handler: Add tests for error handlers
    Add unit tests for the handlers.
    
    Signed-off-by: Anderson Toshiyuki Sasaki <ansasaki@redhat.com>

--- src/errors_handler.rs
+++ src/errors_handler.rs
@@ -239,3 +239,205 @@ pub(crate) fn path_parser_error(err: PathError, req: &HttpRequest) -> Error {
         .json(JsonWrapper::error(400, err))
         .into()
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use actix_web::{test, App, Resource};
+    use core::future::Future;
+    use serde::{Deserialize, Serialize};
+    use serde_json::{json, Value};
+
+    async fn test_default(resource: Resource, allow: &str) {
+        let mut app = test::init_service(App::new().service(resource)).await;
+
+        if allow.contains("GET") {
+            let req = test::TestRequest::get().uri("/").to_request();
+
+            let resp = test::call_service(&mut app, req).await;
+            assert!(resp.status().is_client_error());
+
+            let result: JsonWrapper<Value> = test::read_body_json(resp).await;
+
+            assert_eq!(result.results, json!({}));
+            assert_eq!(result.code, 400);
+        }
+
+        if allow.contains("POST") {
+            let req = test::TestRequest::post()
+                .uri("/")
+                .data("some data")
+                .to_request();
+
+            let resp = test::call_service(&mut app, req).await;
+            assert!(resp.status().is_client_error());
+
+            let result: JsonWrapper<Value> = test::read_body_json(resp).await;
+
+            assert_eq!(result.results, json!({}));
+            assert_eq!(result.code, 400);
+        }
+
+        let req = test::TestRequest::delete().uri("/").to_request();
+
+        let resp = test::call_service(&mut app, req).await;
+        assert!(resp.status().is_client_error());
+
+        let headers = resp.headers();
+
+        assert!(headers.contains_key("allow"));
+        assert_eq!(headers.get("allow").unwrap().to_str().unwrap(), allow); //#[allow_ci]
+
+        let result: JsonWrapper<Value> = test::read_body_json(resp).await;
+
+        assert_eq!(result.results, json!({}));
+        assert_eq!(result.code, 405);
+    }
+
+    #[actix_rt::test]
+    async fn test_app_default() {
+        test_default(web::resource("/").to(app_default), "GET, POST").await
+    }
+
+    #[actix_rt::test]
+    async fn test_api_default() {
+        test_default(web::resource("/").to(api_default), "GET, POST").await
+    }
+
+    #[actix_rt::test]
+    async fn test_keys_default() {
+        test_default(web::resource("/").to(keys_default), "GET, POST").await
+    }
+
+    #[actix_rt::test]
+    async fn test_quotes_default() {
+        test_default(web::resource("/").to(quotes_default), "GET").await
+    }
+
+    #[actix_rt::test]
+    async fn test_notifications_default() {
+        test_default(web::resource("/").to(notifications_default), "POST")
+            .await
+    }
+
+    #[derive(Serialize, Deserialize)]
+    struct DummyQuery {
+        param: String,
+    }
+
+    #[derive(Serialize, Deserialize)]
+    struct DummyPayload {
+        field: u32,
+    }
+
+    #[derive(Serialize, Deserialize)]
+    struct DummyPathParam {
+        number: u32,
+        string: String,
+    }
+
+    async fn dummy(
+        req: HttpRequest,
+        body: web::Json<DummyPayload>,
+        query: web::Query<DummyQuery>,
+    ) -> impl Responder {
+        HttpResponse::Ok().await
+    }
+
+    async fn dummy_with_path(
+        req: HttpRequest,
+        path: web::Path<DummyPathParam>,
+    ) -> impl Responder {
+        HttpResponse::Ok().await
+    }
+
+    #[actix_rt::test]
+    async fn test_parsing_error() {
+        let mut app = test::init_service(
+            App::new()
+                .app_data(
+                    web::JsonConfig::default()
+                        .error_handler(json_parser_error),
+                )
+                .app_data(
+                    web::QueryConfig::default()
+                        .error_handler(query_parser_error),
+                )
+                .app_data(
+                    web::PathConfig::default()
+                        .error_handler(path_parser_error),
+                )
+                .service(
+                    web::resource("/v2.0/ok").route(web::get().to(dummy)),
+                )
+                .service(
+                    web::resource("/v2.0/ok/{number}/{string}")
+                        .route(web::get().to(dummy_with_path)),
+                )
+                .service(
+                    web::resource(r"/v{major:\d+}.{minor:\d+}{tail}*")
+                        .to(version_not_supported),
+                ),
+        )
+        .await;
+
+        // Sanity well formed request
+        let req = test::TestRequest::get()
+            .uri("/v2.0/ok?param=Test")
+            .set_json(&DummyPayload { field: 42 })
+            .to_request();
+
+        let resp = test::call_service(&mut app, req).await;
+
+        assert!(resp.status().is_success());
+
+        // Test unsupported version
+        let req = test::TestRequest::get()
+            .uri("/v500.440/some/tail")
+            .set_json(&DummyPayload { field: 42 })
+            .to_request();
+        let resp = test::call_service(&mut app, req).await;
+        assert!(resp.status().is_client_error());
+        let result: JsonWrapper<Value> = test::read_body_json(resp).await;
+        assert_eq!(result.results, json!({}));
+        assert_eq!(result.code, 400);
+        assert_eq!(result.status, "API version not supported: v500.440");
+
+        // Test JSON parsing error
+        let req = test::TestRequest::get()
+            .uri("/v2.0/ok?param=Test")
+            .header(http::header::CONTENT_TYPE, "application/json")
+            .set_payload("Not JSON")
+            .to_request();
+        let resp = test::call_service(&mut app, req).await;
+        assert!(resp.status().is_client_error());
+        let result: JsonWrapper<Value> = test::read_body_json(resp).await;
+        assert_eq!(result.results, json!({}));
+        assert_eq!(result.code, 400);
+        assert!(result.status.contains("Json deserialize error"));
+
+        // Test Query parsing error
+        let req = test::TestRequest::get()
+            .uri("/v2.0/ok?test=query")
+            .set_json(&DummyPayload { field: 42 })
+            .to_request();
+        let resp = test::call_service(&mut app, req).await;
+        assert!(resp.status().is_client_error());
+        let result: JsonWrapper<Value> = test::read_body_json(resp).await;
+        assert_eq!(result.results, json!({}));
+        assert_eq!(result.code, 400);
+        assert!(result.status.contains("Query deserialize error"));
+
+        // Test Path parsing error
+        let req = test::TestRequest::get()
+            .uri("/v2.0/ok/something/42?test=query")
+            .set_json(&DummyPayload { field: 42 })
+            .to_request();
+        let resp = test::call_service(&mut app, req).await;
+        assert!(resp.status().is_client_error());
+        let result: JsonWrapper<Value> = test::read_body_json(resp).await;
+        assert_eq!(result.results, json!({}));
+        assert_eq!(result.code, 400);
+        assert!(result.status.contains("Path deserialize error"));
+    }
+}
