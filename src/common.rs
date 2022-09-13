873087862673df71058912ae80d518f32235dab8
Anderson Toshiyuki Sasaki <ansasaki@redhat.com>
2022-09-13 15:44:51 +0200

    Do not try to load TPM data generated by another TPM
    Include as part of the stored Agent Data the hash of the public EK of
    the TPM that generated the AK.
    
    Before loading the AK, check if the current TPM is the same TPM that
    generated the information to avoid loading errors.
    
    Signed-off-by: Anderson Toshiyuki Sasaki <ansasaki@redhat.com>

--- src/common.rs
+++ src/common.rs
@@ -186,6 +186,7 @@ pub(crate) struct AgentData {
     pub ak_sign_alg: SignAlgorithm,
     ak_public: Vec<u8>,
     ak_private: Vec<u8>,
+    ek_hash: Vec<u8>,
 }
 
 impl AgentData {
@@ -193,14 +194,17 @@ impl AgentData {
         ak_hash_alg: HashAlgorithm,
         ak_sign_alg: SignAlgorithm,
         ak: &tpm::AKResult,
+        ek_hash: &[u8],
     ) -> Result<Self> {
         let ak_public = ak.public.marshall()?;
         let ak_private: Vec<u8> = ak.private.to_vec();
+        let ek_hash: Vec<u8> = ek_hash.to_vec();
         Ok(Self {
             ak_hash_alg,
             ak_sign_alg,
             ak_public,
             ak_private,
+            ek_hash,
         })
     }
 
@@ -227,8 +231,11 @@ impl AgentData {
         &self,
         hash_alg: HashAlgorithm,
         sign_alg: SignAlgorithm,
+        ek_hash: &[u8],
     ) -> bool {
-        hash_alg == self.ak_hash_alg && sign_alg == self.ak_sign_alg
+        hash_alg == self.ak_hash_alg
+            && sign_alg == self.ak_sign_alg
+            && ek_hash.to_vec() == self.ek_hash
     }
 }
 
