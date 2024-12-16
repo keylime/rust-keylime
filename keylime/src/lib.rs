525c2900102d884f2419474a0b38ff6b2d137b28
Anderson Toshiyuki Sasaki <ansasaki@redhat.com>
2024-12-16 17:13:44 +0100

    keylime: Introduce the registrar_client module
    The registrar_client module implements the builder pattern to allow
    setting the optional parameters as needed.
    
    This also implements the mechanism to allow the agent to communicate
    with the registrar that support different API versions:
    
    - The client will make a GET request to the '/version' endpoint of the
      registrar.  If the request is successful, the client will use the
      provided API version if it is enabled.
    - If the registrar does not support the '/version' endpoint, the client
      will try to register using each of the enabled API versions, starting
      from the latest. If none of the enabled versions is supported by the
      registrar, the registration fails.
    
    This is part of the implementation of the enhancement proposal 114:
    https://github.com/keylime/enhancements/pull/115
    
    Signed-off-by: Anderson Toshiyuki Sasaki <ansasaki@redhat.com>

--- keylime/src/lib.rs
+++ keylime/src/lib.rs
@@ -5,8 +5,10 @@ pub mod hostname_parser;
 pub mod ima;
 pub mod ip_parser;
 pub mod list_parser;
+pub mod registrar_client;
 pub mod serialization;
 pub mod tpm;
+pub mod version;
 
 #[macro_use]
 extern crate static_assertions;
