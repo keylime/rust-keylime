b3166d61d56cb9b1070c7098966c1ea29ce29681
Sergio Arroutbi <sarroutb@redhat.com>
2025-05-12 11:39:59 +0200

    Add Evidence/Authentication messages to prototype
    Signed-off-by: Sergio Arroutbi <sarroutb@redhat.com>

---
+++ keylime-push-model-agent/src/json_dump.rs
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2025 Keylime Authors
+use keylime::structures;
+use serde_json::{Error, Value};
+
+pub fn dump_attestation_request_to_value(
+    request: &structures::AttestationRequest,
+) -> Result<Value, Error> {
+    serde_json::to_value(request)
+}
+
+pub fn dump_session_request_to_value(
+    request: &structures::SessionRequest,
+) -> Result<Value, Error> {
+    serde_json::to_value(request)
+}
+
+pub fn dump_evidence_handling_request_to_value(
+    request: &structures::EvidenceHandlingRequest,
+) -> Result<Value, Error> {
+    serde_json::to_value(request)
+}
