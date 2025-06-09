f17e597b863f102fea4348d5983c96e472e47a4e
Sergio Arroutbi <sarroutb@redhat.com>
2025-06-09 16:10:59 +0200

    Add uefi_log_handler.rs to parse UEFI binary
    Resolves: #1020
    
    Signed-off-by: Sergio Arroutbi <sarroutb@redhat.com>

--- keylime/src/error.rs
+++ keylime/src/error.rs
@@ -112,6 +112,8 @@ pub enum Error {
     CertificateGeneration(
         #[from] crate::crypto::x509::CertificateBuilderError,
     ),
+    #[error("UEFI log parse error")]
+    UEFILog(String),
     #[error("{0}")]
     Other(String),
 }
