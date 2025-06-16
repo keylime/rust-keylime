caff4ab760166e196b7dd679ee08019d3c79abc0
Anderson Toshiyuki Sasaki <ansasaki@redhat.com>
2025-06-16 19:05:42 +0200

    lib/cert: Fix race condition due to use of same file path
    The tests could enter in a race condition where one test removes the
    file while the other is using it. This was caused by the files using the
    same path.
    
    This uses a temporary directory for each test.  This makes it
    unnecessary to manually remove the files and also avoids the risk of
    race conditions since each temporary directory has a unique name.
    
    Signed-off-by: Anderson Toshiyuki Sasaki <ansasaki@redhat.com>

--- keylime/src/cert.rs
+++ keylime/src/cert.rs
@@ -91,62 +91,54 @@ mod tests {
 
     #[test]
     fn test_cert_from_server_key() {
-        const TEST_CERT_PATH: &str = "test_cert.pem";
-        const TEST_KEY_PATH: &str = "test_key.pem";
-        // Ensure the test cert file does not exist before the test
-        let _ = std::fs::remove_file(TEST_CERT_PATH);
-        let _ = std::fs::remove_file(TEST_KEY_PATH);
+        let temp_dir =
+            tempfile::tempdir().expect("failed to create temporary dir");
+        let cert_path = temp_dir.path().join("test_cert.pem");
+        let key_path = temp_dir.path().join("test_key.pem");
         let config = CertificateConfig {
             agent_uuid: "test-uuid".to_string(),
             contact_ip: "1.2.3.4".to_string(),
             contact_port: 8080,
-            server_cert: TEST_CERT_PATH.to_string(),
-            server_key: TEST_KEY_PATH.to_string(),
+            server_cert: cert_path.display().to_string(),
+            server_key: key_path.display().to_string(),
             server_key_password: "test_password".to_string(),
         };
         let result = cert_from_server_key(&config);
         assert!(result.is_ok());
-        // Clean up the test cert file after the test
-        let _ = std::fs::remove_file(TEST_CERT_PATH);
-        let _ = std::fs::remove_file(TEST_KEY_PATH);
     } // test_cert_from_server_key
 
     #[test]
     fn test_cert_no_server_key() {
-        const TEST_CERT_PATH: &str = "test_cert2.pem";
-        // Ensure the test cert file does not exist before the test
-        let _ = std::fs::remove_file(TEST_CERT_PATH);
+        let temp_dir =
+            tempfile::tempdir().expect("failed to create temporary dir");
+        let cert_path = temp_dir.path().join("test_cert.pem");
         let config = CertificateConfig {
             agent_uuid: "test-uuid".to_string(),
             contact_ip: "1.2.3.4".to_string(),
             contact_port: 8080,
-            server_cert: TEST_CERT_PATH.to_string(),
+            server_cert: cert_path.display().to_string(),
             server_key: "".to_string(),
             server_key_password: "test_password2".to_string(),
         };
         let result = cert_from_server_key(&config);
         assert!(result.is_ok());
-        // Clean up the test cert file after the test
-        let _ = std::fs::remove_file(TEST_CERT_PATH);
     } // test_cert_no_server_key
 
     #[test]
     fn test_cert_no_server_cert() {
-        // Ensure the test cert file does not exist before the test
-        const TEST_KEY_PATH: &str = "test_key.pem";
-        let _ = std::fs::remove_file(TEST_KEY_PATH);
+        let temp_dir =
+            tempfile::tempdir().expect("failed to create temporary dir");
+        let key_path = temp_dir.path().join("test_key.pem");
         let config = CertificateConfig {
             agent_uuid: "test-uuid".to_string(),
             contact_ip: "1.2.3.4".to_string(),
             contact_port: 8080,
             server_cert: "".to_string(),
-            server_key: TEST_KEY_PATH.to_string(),
+            server_key: key_path.display().to_string(),
             server_key_password: "test_password".to_string(),
         };
         let result = cert_from_server_key(&config);
         assert!(result.is_ok());
-        // Clean up the test key file after the test
-        let _ = std::fs::remove_file(TEST_KEY_PATH);
     } // test_cert_no_server_cert
 
     #[test]
@@ -165,23 +157,19 @@ mod tests {
 
     #[test]
     fn test_cert_correct_server_key_path() {
-        const TEST_KEY_PATH: &str = "test_key2.pem";
-        const TEST_CERT_PATH: &str = "test_cert4.pem";
-        // Ensure the test key file does not exist before the test
-        let _ = std::fs::remove_file(TEST_KEY_PATH);
-        let _ = std::fs::remove_file(TEST_CERT_PATH);
+        let temp_dir =
+            tempfile::tempdir().expect("failed to create temporary dir");
+        let cert_path = temp_dir.path().join("test_cert.pem");
+        let key_path = temp_dir.path().join("test_key.pem");
         let config = CertificateConfig {
             agent_uuid: "test-uuid".to_string(),
             contact_ip: "1.2.3.4".to_string(),
             contact_port: 8080,
-            server_cert: TEST_CERT_PATH.to_string(),
-            server_key: TEST_KEY_PATH.to_string(),
+            server_cert: cert_path.display().to_string(),
+            server_key: key_path.display().to_string(),
             server_key_password: "test_password4".to_string(),
         };
         let result = cert_from_server_key(&config);
         assert!(result.is_ok());
-        // Clean up files after the test
-        let _ = std::fs::remove_file(TEST_KEY_PATH);
-        let _ = std::fs::remove_file(TEST_CERT_PATH);
     } // test_cert_correct_server_key_path
 }
